f5679fa087947c58d20ce93b990d31249225f052
Asadullah Zahid <asadullahzahid3@gmail.com>
2021-05-30 04:38:29 -0700

    Work Completed.

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,47 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-          home:(Scaffold(body: Text("Asadullah Zahid"))),      
+          home:Scaffold(
+             
+           appBar: AppBar(   
+             title: Center(child: Text("Login Page")),
+             backgroundColor: Colors.deepOrangeAccent,
+             shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(360)),
+           ),
+            body:Center(
+              child: Column(
+                children: [
+                  SizedBox(height: 50,),
+                  Container(
+                    width:200,
+                    child:TextFormField(
+  decoration: InputDecoration(
+    border: UnderlineInputBorder(),
+    labelText: 'Enter your username',
+  ),
+                    )
+                   ),
+
+
+                    SizedBox(height: 50,),
+                  Container(
+                    width:200,
+                    child:TextFormField(
+  decoration: InputDecoration(
+    border: UnderlineInputBorder(),
+    labelText: 'Enter your password',
+  ),
+)
+                   ),
+                   SizedBox(height: 30,),
+                   ElevatedButton(onPressed: (){}, child: Text("Login"))
+
+                ],
+                 
+              ),
+            )
+                
+            )                   
     );
   }
 }
